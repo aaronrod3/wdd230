7c363d738e37a35f79aebc607dcb9b37552a1130
Aaron <aaronrod@me.com>
2023-03-14 11:30:18 -0400

    completed lesson9

---
+++ lesson9/scripts/prophets.js
@@ -0,0 +1,40 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+const displayProphets = (prophets) => {
+    const cards = document.querySelector('div.cards');
+
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let birthdate = document.createElement('h3');
+        let birthplace = document.createElement('h3');
+        let portrait = document.createElement('img');
+
+        h2.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portait of ${prophet.name}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(h2);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    })
+}
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
+    displayProphets(data.prophets);
+  }
+  
+  getProphetData();
+  

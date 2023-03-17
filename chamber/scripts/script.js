8d6dd9414665c0b47090b8048a4852d1f84a0780
Aaron <aaronrod@me.com>
2023-03-17 18:24:45 -0400

    added weather api

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -7,41 +7,40 @@ let date = new Date(document.lastModified);
 document.getElementById("lastUpdated").innerHTML = date;
 
 
-const weatherurl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/sneads%20ferry?unitGroup=us&key=2SXKXZKSWFX9W76BJP5HC2BDC&contentType=json";
+const weatherurl = "https://api.openweathermap.org/data/2.5/weather?zip=28460&units=imperial&appid=d49c0083f5d144998a8f49e0ac248f05";
 
-const displayBusiness = (businesses) => {
-    const cards = document.querySelector('div.cards');
-
-    businesses.forEach((business) => {
-        let card = document.createElement('section');
-        
-        let name = document.createElement('h2');
-        let address = document.createElement('h2');
-        let phone = document.createElement('h3');
-        let website = document.createElement('h3');
-        
-        name.textContent = `${business.name}`;
-        address.textContent = `${business.address}`;
-        phone.textContent = `${business.phonenumber}`;
-        website.textContent = `${business.url}`;
-
-        card.appendChild(name);
-        card.appendChild(address);
-        card.appendChild(phone);
-        card.appendChild(website);
-
-        cards.appendChild(card);
-    })
-}
+const currentTemp = document.querySelector('#currenttemp');
+const condition = document.querySelector('#condition');
+const lowtemp = document.querySelector('#lowtemp');
+const hightemp = document.querySelector('#hightemp');
+const humidity = document.querySelector('#humidity');
 
 async function getWeatherData() {
-    const response = await fetch(weatherurl);
-    const data = await response.json();
-    console.table(data.businesses);  // note that we reference the prophet array of the data object given the structure of the json file
-    displayBusiness(data.businesses);
-  }
+    try {
+        const response = await fetch(weatherurl);
+        if (response.ok) {
+          const data = await response.json();
+        //console.log(data); // this is for testing the call
+        displayWeather(data);
+        } else {
+            throw Error(await response.text());
+        }
+      } catch (error) {
+          console.log(error);
+      }
+    }
   
  getWeatherData();
+
+function displayWeather(weatherData) {
+    currentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)}°F`;
+    condition.innerHTML = `${weatherData.weather[0].description}`;
+    lowtemp.innerHTML = `Low: ${weatherData.main.temp_min.toFixed(0)}°F`;
+    hightemp.innerHTML = `High: ${weatherData.main.temp_max.toFixed(0)}°F`;
+    humidity.innerHTML = `Humidity: ${weatherData.main.humidity.toFixed(0)}`;
+}
+
+
 
 
 // displaying time between visits to site
@@ -100,7 +99,7 @@ const displayBusiness = (businesses) => {
 async function getBusinessData() {
     const response = await fetch(businessurl);
     const data = await response.json();
-    console.table(data.businesses);  // note that we reference the prophet array of the data object given the structure of the json file
+    // console.table(data.businesses);  // note that we reference the prophet array of the data object given the structure of the json file
     displayBusiness(data.businesses);
   }
   

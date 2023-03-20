0cfa8f6a77c9a43a0c7119a876e3fa9bd8750a01
Aaron <aaronrod@me.com>
2023-03-20 11:02:55 -0400

    editing menu

--- chamber/scripts/shareable.js
+++ chamber/scripts/shareable.js
@@ -5,3 +5,14 @@ function clickMenu(x) {
 
 let date = new Date(document.lastModified);
 document.getElementById("lastUpdated").innerHTML = date;
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function clickMenu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
